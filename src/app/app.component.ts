3884b81495384a015809999becaef1aff4fb1de8
Najlae Lambaraa <Najlae.LAMBARAA@etu.uca.fr>
2024-02-20 09:59:30 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,14 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <div class="p-2 w-full flex gap-2 bg-sky-500">
+  <h1 class="text-2xl text-white">Saisie d'inventaire</h1>
+  <a class="hover:text-sky-200" routerLink="/messenger">Saisie</a>
+  <a class="hover:text-sky-200" routerLink="/project">inventaire</a>
+  </div>
+  <router-outlet></router-outlet>
+`
 })
 export class AppComponent {
   title = 'saisie-inventaire';
